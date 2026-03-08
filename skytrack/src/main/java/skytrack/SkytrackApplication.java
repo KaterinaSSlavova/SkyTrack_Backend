78e3d9083ce4af0c72dbc8e8d689d22d551f62dc
KaterinaSSlavova <k.slavova@student.fontys.nl>
2026-03-08 22:49:06 +0100

    initial project setup

---
+++ skytrack/src/main/java/skytrack/SkytrackApplication.java
@@ -0,0 +1,13 @@
+package skytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkytrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkytrackApplication.class, args);
+    }
+
+}
---
+++ skytrack/src/test/java/skytrack/SkytrackApplicationTests.java
@@ -0,0 +1,13 @@
+package skytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkytrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
