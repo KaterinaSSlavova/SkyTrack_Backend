ed7e1daa99f405e2500267889726bb571938cf74
KaterinaSSlavova <k.slavova@student.fontys.nl>
2026-03-26 00:50:05 +0100

    force flyway off in test

--- src/test/java/skytrack/SkytrackApplicationTests.java
+++ src/test/java/skytrack/SkytrackApplicationTests.java
@@ -3,9 +3,11 @@ package skytrack;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = "spring.flyway.enabled=false")
 class SkytrackApplicationTests {
 
     @Test
